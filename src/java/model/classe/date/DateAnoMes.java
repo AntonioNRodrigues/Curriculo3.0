01a483ce8b8145d4306dd8ea14cbf182406c3593
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2015-08-18 17:52:36 +0100

    class DateAnoMes - is a class that has the year attribute and month attribute. Has the implementation of his comparator

---
+++ src/java/model/classe/date/DateAnoMes.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.classe.date;
+
+/**
+ *
+ * @author antonio
+ */
+public class DateAnoMes extends Date {
+
+    public DateAnoMes(int month, int year) {
+        super(month, year);
+    }
+
+    @Override
+    public int compareTo(Date date) {
+        int resulCompare = 0;
+        if (!(date instanceof DateAnoMes)) {
+            throw new UnsupportedOperationException(
+                    "You are comparing a DateAnoMes instance with someting that "
+                    + "is not ");
+        } else {
+            Integer ano = this.getYear();
+            Integer anoDate = date.getYear();
+            resulCompare = ano.compareTo(anoDate);
+            if (resulCompare == 0) {
+                Integer mes = this.getMonth();
+                Integer mesDate = date.getMonth();
+                resulCompare = mes.compareTo(mesDate);
+            }
+        }
+        return resulCompare;
+    }
+
+}
