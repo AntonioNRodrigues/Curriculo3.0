7ad2721cde37f3a5715892493e0cab157209eda5
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2015-08-31 22:21:49 +0100

    minor changes

--- src/java/model/classe/education/EductionUniversitary.java
+++ src/java/model/classe/education/EductionUniversitary.java
@@ -20,4 +20,5 @@ public class EductionUniversitary extends Education {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    
 }
